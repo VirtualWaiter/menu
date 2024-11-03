822d113d2f90255687071eab2442822140bcdd1d
SamuelFumeroHdez <samuelfumerohdez@gmail.com>
2024-11-03 22:06:20 +0100

    endpoint example

---
+++ src/main/java/com/virtualwaiter/menus/controller/ControllerSaludo.java
@@ -0,0 +1,16 @@
+package com.virtualwaiter.menus.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/saludo")
+public class ControllerSaludo {
+
+  @GetMapping("/hola-mundo")
+  public String retrieveSaludo(){
+    return "Hola mundo!";
+  }
+
+}
